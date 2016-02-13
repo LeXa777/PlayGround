5c096d28dc5f6a93c85aa01af4c4f1fafeddaf74
Alex <akrim777@gmail.com>
2016-02-13 14:26:34 +0000

    added tips fetching from the remote server

--- TheWorld/src/TheWorld/wwwroot/js/tripsController.js
+++ TheWorld/src/TheWorld/wwwroot/js/tripsController.js
@@ -4,20 +4,21 @@
     angular.module("app-trips")
         .controller("tripsController", tripsController);
 
-    function tripsController() {
+    function tripsController($http) {
         var vm = this;
 
-        vm.trips = [
-            {
-                name: "US Trip",
-                created: new Date()
-            }, {
-                name: "World Trip",
-                created: new Date()
-            }
-        ];
+        vm.trips = [];
 
         vm.newTrip = {};
+
+        vm.errorMessage = "";
+
+        $http.get("/api/trips")
+            .then(function(response) {
+                angular.copy(response.data, vm.trips);
+            }, function(error) {
+                vm.errorMessage = "Failed to load date: " + error;
+            });
 
         vm.addTrip = function () {
             vm.trips.push({
